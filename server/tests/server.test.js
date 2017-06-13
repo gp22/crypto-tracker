5e26922983b08ea0c47c2b8b831a2ad6e21afa0d
gp22 <gp22@tutanota.com>
2017-06-13 13:01:06 +0700

    Add tests for duplicate currency pairs and invalid post data

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,7 @@
+const { CurrencyPair } = require('./../models/currencyPair');
+const { app } = require('./../server');
 const request = require('supertest');
 const expect = require('expect');
-
-const { app } = require('./../server');
 
 /*
 Verify that we get a homepage
@@ -18,7 +18,10 @@ describe('GET /', () => {
 /*
 Verify that we can add a currency pair to the db
 */
-describe('POST /api/currency', () => {
+describe('POST /api/currency', function () {
+  // Disable this when testing with MLAB db
+  this.timeout(3000);
+
   it('should create a new currency pair', (done) => {
     const currency1 = 'USDT';
     const currency2 = 'BTC';
@@ -36,7 +39,53 @@ describe('POST /api/currency', () => {
         if (err) {
           return done(err);
         }
-        done();
+
+        CurrencyPair.findById(res.body._id).then((foundCurrencyPair) => {
+          expect(foundCurrencyPair.currencyPair).toBe(`${currency1}_${currency2}`);
+          done();
+        }).catch(error => done(error));
+      });
+  });
+
+  it('should not create a duplicate currency pair', (done) => {
+    const currency1 = 'USDT';
+    const currency2 = 'BTC';
+    const start = '1495874524';
+
+    request(app)
+      .post('/api/currency')
+      .send({ currency1, currency2, start })
+      .expect(400)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        CurrencyPair.find().then((currencyPairs) => {
+          expect(currencyPairs.length).toBe(1);
+          done();
+        }).catch(error => done(error));
+      });
+  });
+
+  it('should not create a currency pair with invalid data', (done) => {
+    const currency1 = '';
+    const currency2 = 'BTC';
+    const start = '1495874524';
+
+    request(app)
+      .post('/api/currency')
+      .send({ currency1, currency2, start })
+      .expect(400)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        CurrencyPair.find().then((currencyPairs) => {
+          expect(currencyPairs.length).toBe(1);
+          done();
+        }).catch(error => done(error));
       });
   });
 });
