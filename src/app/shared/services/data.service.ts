a9159fea532116abeadd0ba9aca6c6bf62c3fc61
gp22 <gp22@tutanota.com>
2017-06-20 14:52:16 +0700

    Add basic data service functions

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
+  private chart = {};
 
-  constructor() { }
+  getChart() {
+    return this.chart;
+  }
 
+  addCurrencyPair(currencyPair) {
+    this.chart.currencyPairs.push(currencyPair);
+  }
+
+  deleteCurrencyPair(currencyPairToDelete) {
+    const index = this.chart.currencyPairs.findIndex((element) => {
+      return element.currencyPair === currencyPairToDelete;
+    });
+
+    this.chart.currencyPairs.splice(index, 1);
+  }
 }
