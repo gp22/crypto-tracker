bd315a385db13a65f621eb6128927bd5e87297a1
gp22 <gp22@tutanota.com>
2017-06-22 12:43:28 +0700

    Add event to build initial chart on app load

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,54 @@
 import '../css/main.scss';
 
+const moment = require('moment');
 const Chart = require('chart.js');
 const io = require('socket.io-client');
 const dataService = require('./dataService');
 
 const socket = io();
 
-  // this.socket.emit('updateDateRange');
-  // this.socket.emit('deleteCurrency', { currencyPair: 'USDT_BTC' });
-  // this.socket.emit('addCurrency', { currencyPair: 'USDT_BTC' });
-
 const ctx = document.getElementById('cryptoChart');
-
-const cryptoChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-  }
-});
+const labels = [];
+const datasets = [];
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+};
 
 socket.on('newChart', (chartData) => {
+  labels.length = 0;
+  datasets.length = 0;
+
+  chartData.currencyPairs[0].data.forEach((data) => {
+    const date = (moment.unix(data.date).format('YYYY.MM.DD'));
+    labels.push(date);
+  });
+
+  chartData.currencyPairs.forEach((currencyPair) => {
+    const label = currencyPair.currencyPair;
+    const data = [];
+    const dataSet = ({
+      label,
+      data,
+      borderWidth: 1,
+      fill: false,
+    });
+
+    currencyPair.data.forEach((currencyData) => {
+      data.push(currencyData.high);
+    });
+
+    datasets.push(dataSet);
+  });
   // this.chartData = this.dataService.createChart(chartData);
+  const cryptoChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets,
+    },
+    options,
+  });
 });
 
 socket.on('addCurrency', (newCurrencyPair) => {
